02ef9a0bb03e1ec918f79e39f688f4267c14c0af
Amir Sadri <amirsadriofficial@gmail.com>
2024-02-04 18:21:17 +0330

    question 24 is solved

--- Hard/Memoize-II/Answer.js
+++ Hard/Memoize-II/Answer.js
@@ -5,28 +5,28 @@
 
 const RES = Symbol("result");
 function memoize(fn) {
-    const globalCache = new Map();
-    return (...params) => {
-        let currentCache = globalCache;
-        for (const param of params) {
-            if (!currentCache.has(param)) {
-                currentCache.set(param, new Map());
-            }
-            currentCache = currentCache.get(param);
-        }
-        if (currentCache.has(RES)) return currentCache.get(RES);
-        const result = fn(...params);
-        currentCache.set(RES, result);
-        return result;
+  const globalCache = new Map();
+  return (...params) => {
+    let currentCache = globalCache;
+    for (const param of params) {
+      if (!currentCache.has(param)) {
+        currentCache.set(param, new Map());
+      }
+      currentCache = currentCache.get(param);
     }
+    if (currentCache.has(RES)) return currentCache.get(RES);
+    const result = fn(...params);
+    currentCache.set(RES, result);
+    return result;
+  };
 }
 
 // Example
 let callCount = 0;
 const memoizedFn = memoize(function (a, b) {
-    callCount += 1;
-    return a + b;
-})
-memoizedFn(2, 3) // 5
-memoizedFn(2, 3) // 5
-console.log(callCount) // 1 
+  callCount += 1;
+  return a + b;
+});
+memoizedFn(2, 3); // 5
+memoizedFn(2, 3); // 5
+console.log(callCount); // 1
