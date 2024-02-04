6c99756339688b2e1b13ac25a91249d17bae172a
Amir Sadri <amirsadriofficial@gmail.com>
2024-02-04 17:46:49 +0330

    question 22 is solved

--- Hard/Design-Cancellable-Function/Answer.js
+++ Hard/Design-Cancellable-Function/Answer.js
@@ -1,30 +1,32 @@
 /**
  * @param {Generator} generator
  * @return {[Function, Promise]}
-*/
+ */
 
 function cancellable(generator) {
-    let cancel;
-    const cancelPromise = new Promise((_, reject) => { cancel = () => reject("Cancelled"); });
-    cancelPromise.catch(() => { });
-    const promise = (async () => {
-        let next = generator.next();
-        while (!next.done) {
-            try {
-                next = generator.next(await Promise.race([next.value, cancelPromise]));
-            } catch (e) {
-                next = generator.throw(e);
-            }
-        }
-        return next.value;
-    })();
-    return [cancel, promise];
-};
+  let cancel;
+  const cancelPromise = new Promise((_, reject) => {
+    cancel = () => reject("Cancelled");
+  });
+  cancelPromise.catch(() => {});
+  const promise = (async () => {
+    let next = generator.next();
+    while (!next.done) {
+      try {
+        next = generator.next(await Promise.race([next.value, cancelPromise]));
+      } catch (e) {
+        next = generator.throw(e);
+      }
+    }
+    return next.value;
+  })();
+  return [cancel, promise];
+}
 
 // Example
 function* tasks() {
-  const val = yield new Promise(resolve => resolve(2 + 2));
-  yield new Promise(resolve => setTimeout(resolve, 100));
+  const val = yield new Promise((resolve) => resolve(2 + 2));
+  yield new Promise((resolve) => setTimeout(resolve, 100));
   return val + 1;
 }
 const [cancel, promise] = cancellable(tasks());
