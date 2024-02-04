62e2be9477cca24fd3011740c7166019e64aa667
Amir Sadri <amirsadriofficial@gmail.com>
2024-02-04 17:52:41 +0330

    question 23 is solved

--- Medium/Memoize/Answer.js
+++ Medium/Memoize/Answer.js
@@ -4,26 +4,25 @@
 */
 
 function memoize(fn) {
-    const cache = {};
-    return function (...args) {
-        const key = JSON.stringify(args);
-        if (key in cache) {
-            return cache[key];
-        }
-        const result = fn.apply(this, args);
-        cache[key] = result;
-        return result;
+  const cache = {};
+  return function (...args) {
+    const key = JSON.stringify(args);
+    if (key in cache) {
+      return cache[key];
     }
-
+    const result = fn.apply(this, args);
+    cache[key] = result;
+    return result;
+  };
 }
 
 // Example
 let callCount = 0;
 const memoizedFn = memoize(function (a, b) {
-    callCount += 1;
-    return a + b;
-})
-memoizedFn(2, 3) // 5
-memoizedFn(2, 3) // 5
-memoizedFn(2, 3) // 5
-console.log(callCount) // 1 
+  callCount += 1;
+  return a + b;
+});
+memoizedFn(2, 3); // 5
+memoizedFn(2, 3); // 5
+memoizedFn(2, 3); // 5
+console.log(callCount); // 1
