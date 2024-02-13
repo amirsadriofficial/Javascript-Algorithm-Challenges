864e3b5ba5e09ca3c2dd30f0fb598de40c8b466a
Amir Sadri <amirsadriofficial@gmail.com>
2024-02-13 11:58:25 +0330

    question 37 is solved

--- Medium/Cache-With-Time-Limit/Answer.js
+++ Medium/Cache-With-Time-Limit/Answer.js
@@ -1,28 +1,28 @@
 const TimeLimitedCache = function () {
-    this.cache = {};
-    this.activeKeysCounter = 0;
+  this.cache = {};
+  this.activeKeysCounter = 0;
 };
 TimeLimitedCache.prototype.set = function (key, value, duration) {
-    var keyInCache = this.cache[key];
-    keyInCache ? clearTimeout(keyInCache.timeout) : this.activeKeysCounter++;
-    this.cache[key] = {
-        value: value,
-        timeout: setTimeout(() => {
-            this.cache[key].value = -1;
-            this.activeKeysCounter--;
-        }, duration),
-    }
-    return Boolean(keyInCache);
+  var keyInCache = this.cache[key];
+  keyInCache ? clearTimeout(keyInCache.timeout) : this.activeKeysCounter++;
+  this.cache[key] = {
+    value: value,
+    timeout: setTimeout(() => {
+      this.cache[key].value = -1;
+      this.activeKeysCounter--;
+    }, duration),
+  };
+  return Boolean(keyInCache);
 };
 TimeLimitedCache.prototype.get = function (key) {
-    return this.cache[key]?.value || -1;
+  return this.cache[key]?.value || -1;
 };
 TimeLimitedCache.prototype.count = function () {
-    return this.activeKeysCounter;
+  return this.activeKeysCounter;
 };
 
 // Example
-const timeLimitedCache = new TimeLimitedCache()
+const timeLimitedCache = new TimeLimitedCache();
 timeLimitedCache.set(1, 42, 1000); // false
-timeLimitedCache.get(1) // 42
-timeLimitedCache.count() // 1
+timeLimitedCache.get(1); // 42
+timeLimitedCache.count(); // 1
