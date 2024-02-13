ed035b620da733f8d29aea362e6e5854adf6a02b
Amir Sadri <amirsadriofficial@gmail.com>
2024-02-13 12:04:48 +0330

    question 38 is solved

--- Medium/Check-if-Object-Instance-of-Class/Answer.js
+++ Medium/Check-if-Object-Instance-of-Class/Answer.js
@@ -2,13 +2,12 @@
  * @param {*} obj
  * @param {*} classFunction
  * @return {boolean}
-*/
+ */
 
 const checkIfInstanceOf = function (obj, classFunction) {
-    if (obj == null || typeof classFunction != 'function')
-        return false;
-    let val = (Object(obj) instanceof classFunction);
-    return val;
+  if (obj == null || typeof classFunction != "function") return false;
+  let val = Object(obj) instanceof classFunction;
+  return val;
 };
 
 // Example
